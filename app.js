416b6f1328db7228fb1ab3bd6b90569f293ba39c
Steven <steven.jasionowicz@gmail.com>
2017-05-07 20:12:50 +1000

    First commit

---
+++ app.js
@@ -0,0 +1,40 @@
+/*
+var time = 0;
+
+var timer = setInterval(function(){
+	time += 2;
+	console.log(time + ' seconds have passed');
+	if (time > 5) {
+		clearInterval(timer);
+	}
+}, 2000);
+*/
+
+/*
+console.log(__dirname);
+console.log(__filename);
+*/
+
+/*normal function statement
+function sayHi() {
+	console.log("Hi");
+}
+sayHi();
+*/
+
+/*
+var sayBye = function(){
+	console.log("Bye");
+}
+sayBye();
+
+function callFunction(fun) {
+	fun();
+}
+
+callFunction(sayBye);
+*/
+
+var counter = require('./count');
+
+console.log(counter(['Pastor', 'Senna', 'Jules', 'Prost']));
---
+++ count.js
@@ -0,0 +1,6 @@
+var counter = function(arr){
+	return 'There are ' + arr.length + ' F1 drivers in this array';
+};
+
+module.exports = counter;
+
